6478a47a4df1836eab24f30415ee2d8b351ac910
gus-aguiar <gustavopma90@gmail.com>
2023-09-03 16:54:19 -0300

    req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,16 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        response.raise_for_status()
+        return response.text
+    except Exception:
+        return None
 
 
 # Requisito 2
