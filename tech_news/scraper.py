68ad21003fdc1e0b7156a620022c52fb57ef0b3d
gus-aguiar <gustavopma90@gmail.com>
2023-09-04 18:18:12 -0300

    req5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -30,14 +31,44 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel=canonical]::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get().strip()
+    date = selector.css(".meta-date::text").get()
+    author = selector.css(".author a::text").get()
+    reading_time = int(
+        selector.css(".meta-reading-time::text").re_first(r"\d+")
+    )
+    summary = "".join(
+        selector.css(".entry-content > p:first-of-type *::text").getall()
+    ).strip()
+    category = selector.css(".category-style .label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": date,
+        "writer": author,
+        "reading_time": reading_time,
+        "summary": summary,
+        "category": category,
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    site_content = fetch("https://blog.betrybe.com/")
+    list_news = scrape_updates(site_content)
+    news = []
 
+    for idx in range(amount):
+        if idx % len(list_news) == 0:
+            site_content = fetch(scrape_next_page_link(site_content))
+            list_news.extend(scrape_updates(site_content))
+        if idx < len(list_news):
+            news_content = fetch(list_news[idx])
+            if news_content:
+                news.append(scrape_news(news_content))
 
-# initial commit
+    create_news(news)
+
+    return news
